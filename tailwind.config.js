aaed2ecd3ce4cbf266debc6347b8448058b9a8ad
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-09-23 10:01:15 -0400

    Add config files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/*.{html,pug,js}',
+    './public/**/*.{html,pug,js}',
+  ],
+  future: {},
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
